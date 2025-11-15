00665efd29ec60a60ffd658d48558cc42a3885e1
yashodha.s <yashodha.s@informaticsint.com>
2025-11-15 23:03:37 +0530

    REPZ:scrum-77:ENHC:added about section animations

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,47 +3,12 @@ import Tabs from "@/components/Tabs";
 import Timeline from "@/components/Timeline";
 import LeadershipCard from "@/components/LeadershipCard";
 import CoreValues from "@/components/CoreValues";
+import AboutHeroSection from "@/components/AboutHero";
 
 export default function AboutPage() {
     return (
         <>
-            <section className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 px-6 py-16 flex flex-col items-center">
-                <h1 className="text-4xl md:text-5xl font-extrabold text-gray-900 text-center mb-6">
-                    Here's who we are <br /> & what our gym is about
-                </h1>
-
-                <button className="mb-16 px-6 py-3 bg-[#CFF500] hover:bg-[#b8e000] text-black font-medium rounded-full shadow hover:shadow-lg transition-all">
-                    ✨ Get to know more about us
-                </button>
-
-                <div className="max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-10 items-center mb-24">
-                    <div className="text-black p-10 rounded-3xl shadow-lg bg-gradient-to-r from-blue-50 to-white">
-                        <h2 className="text-2xl font-semibold mb-4">
-                            So you know about <span className="text-black">FitLife Gym</span> — but what about the
-                            team behind it?
-                        </h2>
-                        <p className="text-gray-800 leading-relaxed">
-                            Meet the passionate trainers, developers, and fitness lovers who
-                            made this platform possible. Our team’s goal is to help you stay
-                            motivated, healthy, and inspired.
-                        </p>
-                        <p className="mt-10 text-center text-black font-semibold text-lg">
-                            #TeamFitLife 💪
-                        </p>
-                    </div>
-
-                    <div className="bg-gradient-to-r from-white to-blue-50 p-8 rounded-3xl shadow-lg flex flex-wrap justify-center gap-4">
-                        {["/team1.jpg", "/team2.jpg", "/team3.jpg", "/team4.jpg"].map((src, idx) => (
-                            <div
-                                key={idx}
-                                className="relative w-40 h-28 rounded-xl overflow-hidden transform hover:rotate-0 transition"
-                            >
-                                <Image src={src} alt={`Team ${idx + 1}`} fill className="object-cover" />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </section>
+            <AboutHeroSection></AboutHeroSection>
 
             {/* ---- Tabs Section ---- */}
             <Tabs
